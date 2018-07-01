f935bcab31aa25e7149b6aca1d20edf51f5ed4b6
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-07-01 22:39:16 +0700

    finished release 0

--- models/student.js
+++ models/student.js
@@ -1,12 +1,14 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var Teacher = sequelize.define('Teacher', {
+  var Student = sequelize.define('Student', {
     first_name: DataTypes.STRING,
     last_name: DataTypes.STRING,
     email: DataTypes.STRING,
+    createdAt: new Date(),
+    updatedAt: new Date(),
   }, {});
-  Teacher.associate = function(models) {
+  Student.associate = function(models) {
     // associations can be defined here
   };
-  return Teacher;
+  return Student;
 };
