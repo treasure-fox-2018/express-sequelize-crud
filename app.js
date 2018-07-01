23d55344b2fbb495e881e0f79d0614ffbc0f289d
Odelia <odeliasusanto@gmail.com>
2018-07-02 02:39:10 +0700

    release 0 done. can't fix errors doing release 1

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict'
+const express = require('express');
+const app = express();
+const routes = require('../routes');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use('/', routes);
+
+app.listen('3000', () => {
+    console.log('server started at port 3000')
+})
